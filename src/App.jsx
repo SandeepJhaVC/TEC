fc8c843803e605969a6a96e57e831c2b7464c0da
Sandeep Jha <76050039+SandeepJhaVC@users.noreply.github.com>
2025-01-09 07:58:44 +0000

    basic file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './components/Home.jsx'
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx';
 
-export default function App() {
+function App() {
   return (
-    <main>
-      React ⚛️ + Vite ⚡ + Replit
-    </main>
-  )
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path='/videos' element={<Videos />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} /> */}
+      </Routes>  
+      <Footer />
+    </Router>
+  );
 }
+
+export default App;
