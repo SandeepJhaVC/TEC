01a618be5e2337fa946df0041d7435c7019bc143
Sandeep Jha <sandeepjhavc@gmail.com>
2026-04-09 11:31:56 +0530

    import error resolved

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
